4ab4ddf106b0ef2f6aa584915af209802bb426a3
David Herring <davidherring@Davids-MBP.home>
2023-03-05 20:16:21 -0600

    adding basic plan and wireframe

--- Server/App.js
+++ Server/App.js
@@ -2,7 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 
+// initializing express
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Starting the port
 app.listen(PORT, () => console.log(`Server connected to port: ${PORT}.`));
